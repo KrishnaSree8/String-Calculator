b60cb8e27efa3d1b9da8e05d4eead977ffd90f19
krishnasree8 <krishnaasreee@gmail.com>
2024-09-25 14:20:03 +0530

    Support newlines as delimiters

--- calculator.test.js
+++ calculator.test.js
@@ -1,4 +1,4 @@
-const add = require('./calculator'); // Import add function
+const add = require('./calculator');
 
 test('returns 0 for an empty string', () => {
     expect(add("")).toBe(0);
@@ -10,4 +10,12 @@ test('returns the number when a single number is provided', () => {
 
 test('returns the sum of two numbers separated by a comma', () => {
     expect(add("1,2")).toBe(3);
+});
+
+test('returns the sum of multiple numbers separated by commas', () => {
+    expect(add("1,2,3,4,5")).toBe(15);
+});
+
+test('handles newlines as delimiters along with commas', () => {
+    expect(add("1\n2,3")).toBe(6);
 });
