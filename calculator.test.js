c5d679cdfbb857b91fd113ca3642e5aafe2e40d0
krishnasree8 <krishnaasreee@gmail.com>
2024-09-25 14:26:47 +0530

    Support custom delimiters

--- calculator.test.js
+++ calculator.test.js
@@ -19,3 +19,7 @@ test('returns the sum of multiple numbers separated by commas', () => {
 test('handles newlines as delimiters along with commas', () => {
     expect(add("1\n2,3")).toBe(6);
 });
+
+test('supports custom delimiters', () => {
+    expect(add("//;\n1;2")).toBe(3);
+});
