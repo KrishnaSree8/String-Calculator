6549ed7015fcf7de4951cf7cda021ee5fb7ca5bc
krishnasree8 <krishnaasreee@gmail.com>
2024-09-25 14:14:51 +0530

    Handle single number input

--- calculator.test.js
+++ calculator.test.js
@@ -3,3 +3,7 @@ const add = require('./calculator'); // Import add function
 test('returns 0 for an empty string', () => {
     expect(add("")).toBe(0);
 });
+
+test('returns the number when a single number is provided', () => {
+    expect(add("1")).toBe(1);
+});
